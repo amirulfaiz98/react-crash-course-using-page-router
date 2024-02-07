60b788ac216fe7df2c7111358390fe98ba80870b
Amirul Faiz <faiz@baituljannah.com>
2024-02-07 12:03:25 +0800

    Adding A First Page

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>THe Home Page</h1>
+    </div>
+  );
+}// style 1
+
+// style 2
+// function HomePage() {
+//     return (
+//         <div>
+//           <h1>THe Home Page</h1>
+//         </div>
+//       );
+// }
+
+// export default HomePage
