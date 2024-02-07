5752a3278bbd555d33fffcb5266c8b5991462c97
Amirul Faiz <faiz@baituljannah.com>
2024-02-07 13:10:31 +0800

    Navigating with the "Link" Component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+
 export default function HomePage() {
   return (
     <div>
       <h1>THe Home Page</h1>
+      <ul>
+        <li>
+            <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+            <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 }// style 1
